06a91400b40ee39173d27637faf9201c9e7fe84d
DinghaoLI <dinghaoli666@gmail.com>
2019-03-18 15:34:40 +0800

    Problem 006

--- 005-从尾到头打印链表(ing)/problem005.go
+++ 005-从尾到头打印链表(ing)/problem005.go
@@ -1,33 +1,35 @@
-package problem005
+package main
 
-type struct NodeList{
-	int val
+import (
+	"fmt"
+)
+
+type NodeList struct{
+	Val int
+	Next *NodeList
 }
 
-func replaceSpace(str []byte, length int) {
-	count := 0
-	// 遍历一遍字符串， 统计字符出现的数目, 计算替换后的字符串长度
-	for i:=0; i<length; i++ {
-		if str[i] == ' '{
-			count++
-		}
-	}
-	newlength := length + count*2
+// just print it form tail to head and do not modify the original NodeList
 
-	// 两个index，一个指向length-1, 另一个指向newlength-1，遍历一遍字符串，完成替换
-	for l,nl := length-1,newlength-1; l>=0 && nl>=0; {
-		if str[l]==' '{
-			str[nl] = '0'
-			nl--
-			str[nl] = '2'
-			nl--
-			str[nl] = '%'
-			nl--
-			l--
-		} else {
-			str[nl] = str[l]
-			nl--
-			l--
-		}
+func printListFromTailToHead(head *NodeList) {
+	if head != nil {
+		printListFromTailToHead(head.Next)
+		fmt.Printf("%d -> ", head.Val)
 	}
+}
+
+func main() {
+	// example
+	n3 := &NodeList{3, nil}
+	n2 := &NodeList{2, n3}	
+	n1 := &NodeList{1, n2}
+
+	fmt.Printf("\n NodeList 1 -> 2 -> 3 \n")
+
+	// test
+	fmt.Printf("\n Output: ")
+	printListFromTailToHead(n1)	
+	fmt.Printf("\n \n")
+
+
 }
---
+++ 006-重建二叉树/problem006.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"fmt"
+)
+
+type TreeNode struct{
+	Val int
+	Left *TreeNode
+	Right *TreeNode
+}
+
+func printPreOrder(root *TreeNode){
+	if root != nil {
+		fmt.Printf("%d ", root.Val)
+		printPreOrder(root.Left)
+		printPreOrder(root.Right)
+	}
+}
+
+func printInOrder(root *TreeNode){
+	if root != nil {
+		printInOrder(root.Left)
+		fmt.Printf("%d ", root.Val)
+		printInOrder(root.Right)
+	}
+}
+
+func reConstructBinaryTree(pre []int, in []int) *TreeNode {
+	if len(pre) != len(in) || len(pre) == 0 { 
+		return nil 
+	}
+	// find root and root Index in inOrder
+	rootVal := pre[0]
+	rootIndex := 0
+	for i := 0; i < len(in); i++ {
+		if in[i] == rootVal {
+			rootIndex = i
+		}
+	}
+	inL, inR := in[:rootIndex], in[rootIndex+1:]
+	preL, preR := pre[1:rootIndex+1], pre[rootIndex+1:]
+	left := reConstructBinaryTree(preL, inL)
+	right := reConstructBinaryTree(preR, inR)
+	return &TreeNode{Val: rootVal, Left: left, Right: right}
+}
+
+
+
+func main() {
+	// example
+	pre := []int{1,2,4,7,3,5,6,8}
+	in := []int{4,7,2,1,5,3,6,8}
+
+	fmt.Println("preOder: ", pre)
+	fmt.Println("inOrder: ", in)
+
+	// Reconstruct
+	fmt.Println("\nReconstruct Binary Tree... \n ",)
+	root := reConstructBinaryTree(pre, in)
+
+	// test 
+	fmt.Printf("preOder from Tree reconstructed:  ")
+	printPreOrder(root)
+	fmt.Printf("\n")
+
+	fmt.Printf("inOder from Tree reconstructed:  ")
+	printInOrder(root)
+	fmt.Printf("\n")
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
